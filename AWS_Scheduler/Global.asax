a2c38e4deab2b13e67d534ca49712386834fa83f
paulo.cavalcante <paulo.cavalcante@RCNIFR05>
2016-08-02 15:05:32 -0300

    Add project files.

---
+++ AWS_Scheduler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AWS_Scheduler.MvcApplication" Language="C#" %>
---
+++ AWS_Scheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AWS_Scheduler.Startup))]
+namespace AWS_Scheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
